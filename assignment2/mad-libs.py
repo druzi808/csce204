d246d14bed23c1414cb567813318c72cc5b4c18b
druzi808 <77406840+druzi808@users.noreply.github.com>
2021-01-15 15:30:03 -0500

    added assignment 2- initials.py & mad-libs.py with screenshots

---
+++ assignment2/mad-libs.py
@@ -0,0 +1,17 @@
+#Author: Drew Rafferty
+
+
+college = input("College Name: ")
+adj = input("Adjective: ")
+bodyPart = input("Body Part: ")
+obj = input("Object: ")
+smell = input("Smell: ")
+action = input("Action: ")
+adj2 = input("New Adjective: ")
+
+print (f"""Let's begin shall we?
+It was the one night everybody dreads, the night before dark week starts- AKA finals week- at {college}. 
+The library was full of {adj} students all glued to their books and {bodyPart} deep in energy drink cans and empty coffee cups. 
+One desperate student even had the guts to sneak in a(n) {obj}. 
+As expected, I couldn\'t find a decent place to sit so I had to sit next to the dude who smelled like {smell}. 
+I began to {action} {adj2}.""")

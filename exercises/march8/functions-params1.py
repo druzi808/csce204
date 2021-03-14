a51bfee683a8494b1e528275874fb1080069ea17
druzi808 <77406840+druzi808@users.noreply.github.com>
2021-03-14 18:37:24 -0400

    march 8th with functions and parameters

---
+++ exercises/march8/functions-params1.py
@@ -0,0 +1,6 @@
+#Author: Drew Rafferty
+def sayHello(firstName, lastName):
+    print(f"Hello {firstName} {lastName}")
+    
+sayHello("Portia", "Plante")
+sayHello("Drew", "Rafferty")
---
+++ exercises/march8/functions1.py
@@ -0,0 +1,10 @@
+#Author: Drew Rafferty
+def say_hello():
+    print("Hello World")
+
+def say_goodbye():
+    print("Goodbye")
+
+say_hello()
+say_goodbye()
+say_hello()
